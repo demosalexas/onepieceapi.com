812ea84abc25ddf96bbcf26e9826819316ad4a1b
Demos Alexas <demosalexas@tuta.io>
2024-03-05 15:40:29 -0300

    fix: api data

--- src/app/api/characters/route.ts
+++ src/app/api/characters/route.ts
@@ -1,38 +1,36 @@
 import { NextResponse } from 'next/server'
-import { promises as fs } from 'fs'
 
 import { createPages } from '@/utils/createPages'
 import data from '@/data/characters.json' 
 
 export async function GET(request: Request) {
-  // const file = await fs.readFile(process.cwd() + '/src/data/characters.json', 'utf8')
-  // const data = JSON.parse(file)
-  // console.log(data)
   const url = new URL(request.url)
   const searchParams = new URLSearchParams(url.searchParams)
-  const currentPage = Number(searchParams.get('page'))
+  const currentPage: number | undefined = Number(searchParams.get('page')) 
+  const numberOfCharacters: number = data.length
+  const pages = createPages(data, 20)
+  const numberOfPages: number = Object.keys(pages).length
 
-  // const pages = createPages(data, 20)
-  // console.log(pages)
+  let page: any[] = pages[`page1`]
 
-  if(typeof currentPage === 'number') {
-
+  if(typeof currentPage === 'number' && currentPage <= numberOfPages && currentPage > 0) {
+    page = pages[`page${currentPage}`]
   }
 
-  // Search Params : bounty, age, status, origin ...
+  const baseUrl = 'https://onepieceapi.com/api/characters'
+  const nextUrl = `${baseUrl}?page=${currentPage <= numberOfPages && currentPage > 0 ? currentPage + 1 : 2}`
+  const previousUrl = `${baseUrl}?page=${currentPage <= numberOfPages && currentPage > 0 ? currentPage - 1 : null}`
 
   const info = {
-    "count": 23, // data.length,
-    "pages": 23,
-    "currentPage": Number(searchParams.get('page')),
-    "next": "",
-    "previous": ""
+    characters: numberOfCharacters,
+    pages: numberOfPages,
+    currentPage: currentPage,
+    next: nextUrl,
+    previous: previousUrl,
   }
-
-  // console.log(data.length, "data")
 
   return NextResponse.json({
     info,
-    data: data,
+    data: page,
   })
 }
